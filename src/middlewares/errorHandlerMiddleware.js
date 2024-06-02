68dbc56d0a76c1edfac0db71aac392568f55325b
Natali-1991 <natawa.bodnar@mail.ru>
2024-06-02 20:51:46 +0100

    create contactRouts,add error handling

--- src/middlewares/errorHandlerMiddleware.js
+++ src/middlewares/errorHandlerMiddleware.js
@@ -1,5 +1,32 @@
+import { isHttpError } from 'http-errors';
+import { MongooseError } from 'mongoose';
+
 export const errorHandlerMiddleware = (err, req, res, next) => {
+  if (isHttpError(err)) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: {
+        message: err.message,
+      },
+    });
+  }
+
+  if (err instanceof MongooseError) {
+    return res.status(500).json({
+      status: 500,
+      message: 'Mongoose error',
+      data: {
+        message: err.message,
+      },
+    });
+  }
+
   res.status(500).json({
+    status: 500,
     message: 'Something went wrong',
+    data: {
+      error: err.message,
+    },
   });
 };
--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
 import pino from 'pino-http';
 import express from 'express';
 import cors from 'cors';
-import mongoose from 'mongoose';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
 import { notFoundMiddleware } from './middlewares/notFoundMiddleware.js';
 import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware.js';
 import { ENV_VARS } from './constants/index.js';
+import contactsRouter from './routers/contacts.js';
 
 const app = express();
 const PORT = Number(env(ENV_VARS.PORT, '3000'));
@@ -22,43 +21,7 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
-
-  app.get('/contacts/:contactId', async (req, res) => {
-    try {
-      const { contactId } = req.params;
-
-      if (!mongoose.Types.ObjectId.isValid(contactId)) {
-        return res.status(400).json({ error: 'Invalid ID format' });
-      }
-
-      const contact = await getContactById(contactId);
-
-      if (!contact) {
-        return res.status(404).json({
-          status: 404,
-          message: `User with id ${contactId} not found!`,
-        });
-      }
-
-      res.json({
-        status: 200,
-        message: `Successfully found contact with id ${contactId}!`,
-        data: contact,
-      });
-    } catch (error) {
-      console.error('Error fetching contact:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-
+  app.use(contactsRouter);
   app.use('*', notFoundMiddleware);
 
   app.use(errorHandlerMiddleware);
