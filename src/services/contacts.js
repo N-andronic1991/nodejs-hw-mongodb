1f9ac42b0f2cb434fd8b3a5fb521fb29bee0f5d4
Natali-1991 <natawa.bodnar@mail.ru>
2024-06-24 12:26:14 +0100

    update method

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -41,7 +41,7 @@ export const getAllContacts = async ({
 };
 
 export const getContactById = async (contactId, userId) => {
-  const contact = await Contact.findById(contactId, userId);
+  const contact = await Contact.findOne({ _id: contactId, userId: userId });
   return contact;
 };
 
